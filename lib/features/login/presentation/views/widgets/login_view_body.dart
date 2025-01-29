e9e641027f5ebf2553af6b9dc339940bd8b81a57
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2025-01-29 09:04:51 +0200

    Add login logic and manage states of login

--- lib/features/login/presentation/views/widgets/login_view_body.dart
+++ lib/features/login/presentation/views/widgets/login_view_body.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:healr/core/utils/app_router.dart';
+import 'package:healr/core/utils/service_locator.dart';
 import 'package:healr/core/utils/styles.dart';
 import 'package:healr/core/widgets/custom_button.dart';
-import 'package:healr/features/login/presentation/views/widgets/custom_sign_in_with_email_button.dart';
+import 'package:healr/features/login/data/repos/login_repo_imp.dart';
+import 'package:healr/features/login/presentation/manager/cubit/login_cubit.dart';
 import 'package:healr/core/widgets/custom_text_field.dart';
 import 'package:healr/core/widgets/custom_text_rich.dart';
+import 'package:healr/features/login/presentation/views/widgets/custom_sign_in_with_email_button.dart';
 
 class LoginViewBody extends StatefulWidget {
   const LoginViewBody({super.key});
@@ -17,68 +21,151 @@ class LoginViewBody extends StatefulWidget {
 }
 
 class _LoginViewBodyState extends State<LoginViewBody> {
+  final TextEditingController nationalController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  String? errorMessage;
+
+  @override
+  void dispose() {
+    nationalController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  void login(BuildContext context) {
+    if (formKey.currentState!.validate()) {
+      final nationalId = nationalController.text;
+      final password = passwordController.text;
+
+      setState(() {
+        errorMessage = null;
+      });
+
+      BlocProvider.of<LoginCubit>(context).loginUser(nationalId, password);
+    }
+  }
+
+  String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) return 'Password is required';
+    if (value.length < 8) return 'Password must be at least 8 characters';
+    if (!value.contains(RegExp(r'[A-Z]'))) {
+      return 'Password must contain at least one uppercase letter';
+    }
+    if (!value.contains(RegExp(r'[0-9]'))) {
+      return 'Password must contain at least one number';
+    }
+    if (!value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
+      return 'Password must contain at least one special character';
+    }
+    if (value.length > 15) return 'Password must be at most 15 characters';
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.only(top: 88.h),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text(
-                  textAlign: TextAlign.center,
-                  'Sign in',
-                  style: Styles.textStyle32.copyWith(
-                    fontWeight: FontWeight.w600,
+    return BlocProvider(
+      create: (context) => LoginCubit(getIt.get<LoginRepoImp>()),
+      child: SafeArea(
+        child: Scaffold(
+          body: BlocConsumer<LoginCubit, LoginState>(
+            listener: (context, state) {
+              if (state is LoginSuccess) {
+                GoRouter.of(context).pushReplacement(AppRouter.kSignUpView);
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Login successful ✅'),
+                    duration: Duration(milliseconds: 800),
+                    backgroundColor: Color.fromARGB(255, 13, 79, 127),
+                  ),
+                );
+              } else if (state is LoginFailure) {
+                setState(() {
+                  errorMessage = state.errMessage;
+                });
+              }
+            },
+            builder: (context, state) {
+              return SingleChildScrollView(
+                child: Form(
+                  key: formKey,
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 88.h),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          textAlign: TextAlign.center,
+                          'Sign in',
+                          style: Styles.textStyle32.copyWith(
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        SizedBox(height: 60.h),
+                        CustomTextField(
+                          controller: nationalController,
+                          labelText: 'National number',
+                          hintText: 'Enter your 14-digit national number',
+                          obscureText: false,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'National number is required';
+                            }
+                            if (value.length != 14) {
+                              return 'National number must be 14 digits';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 16.h),
+                        CustomTextField(
+                          controller: passwordController,
+                          onTap: () {
+                            GoRouter.of(context)
+                                .push(AppRouter.kForgetPasswordView);
+                          },
+                          showForgotPass: true,
+                          labelText: 'Password',
+                          hintText: '•••••••••••••••',
+                          obscureText: true,
+                          validator: validatePassword,
+                          errorText: errorMessage,
+                        ),
+                        SizedBox(height: 24.h),
+                        CustomButton(
+                          text:
+                              state is LoginLoading ? 'Loading...' : 'Sign in',
+                          onPressed: state is LoginLoading
+                              ? null
+                              : () => login(context),
+                        ),
+                        SizedBox(height: 16.h),
+                        SvgPicture.asset(
+                          'assets/images/or.svg',
+                          height: 20.h,
+                          width: 20.w,
+                        ),
+                        SizedBox(height: 16.h),
+                        CustomSignInWithEmailButton(
+                          onTap: () {},
+                        ),
+                        SizedBox(height: 16.h),
+                        Center(
+                          child: CustomTextRich(
+                            text1: 'New Here?',
+                            text2: '  Create an Account',
+                            onTapText2: () {
+                              GoRouter.of(context).push(AppRouter.kSignUpView);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                SizedBox(height: 60.h),
-                CustomTextField(
-                  labelText: 'National number',
-                  hintText: 'Enter your 14-digit national number',
-                  onChanged: (value) {},
-                  obscureText: false,
-                ),
-                SizedBox(height: 16.h),
-                CustomTextField(
-                  onTap: () {
-                    GoRouter.of(context).push(AppRouter.kForgetPasswordView);
-                  },
-                  showForgotPass: true,
-                  labelText: 'Password',
-                  hintText: '•••••••••••••••',
-                  onChanged: (value) {},
-                  obscureText: true,
-                ),
-                SizedBox(height: 24.h),
-                CustomButton(
-                  text: 'Sign in',
-                  onPressed: () {},
-                ),
-                SizedBox(height: 16.h),
-                SvgPicture.asset(
-                  'assets/images/or.svg',
-                  height: 20.h,
-                  width: 20.w,
-                ),
-                SizedBox(height: 16.h),
-                CustomSignInWithEmailButton(
-                  onTap: () {},
-                ),
-                SizedBox(height: 16.h),
-                Center(
-                  child: CustomTextRich(
-                    text1: 'New Here?',
-                    text2: '  Create an Account',
-                    onTapText2: () {
-                      GoRouter.of(context).push(AppRouter.kSignUpView);
-                    },
-                  ),
-                ),
-              ],
-            ),
+              );
+            },
           ),
         ),
       ),
