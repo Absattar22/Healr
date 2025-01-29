af0b0316ea368325532ebd841f9e9f774c8b9b75
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2025-01-29 09:02:04 +0200

    Add some improvments to the text field

--- lib/core/widgets/custom_text_field.dart
+++ lib/core/widgets/custom_text_field.dart
@@ -1,155 +1,141 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:healr/core/constants.dart';
 import 'package:healr/core/utils/styles.dart';
-
 class CustomTextField extends StatefulWidget {
-  CustomTextField({
+  const CustomTextField({
     super.key,
     required this.hintText,
     required this.labelText,
-    this.onChanged,
+    required this.controller,
     required this.obscureText,
+    this.onChanged,
     this.showForgotPass = false,
     this.onTap,
+    this.validator,
+    this.errorText,
   });
 
+  final TextEditingController controller;
   final String hintText, labelText;
   final Function(String)? onChanged;
-  bool showPassword = true;
   final bool obscureText;
   final bool showForgotPass;
   final void Function()? onTap;
+  final String? Function(String?)? validator;
+  final String? errorText; 
 
   @override
   State<CustomTextField> createState() => _CustomTextFieldState();
 }
 
 class _CustomTextFieldState extends State<CustomTextField> {
-  final TextEditingController controller = TextEditingController();
+  bool showPassword = true;
 
   @override
   void initState() {
     super.initState();
-    widget.showPassword = widget.obscureText;
+    showPassword = widget.obscureText;
   }
-
-  final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(
-        horizontal: 16.w,
-      ),
+      padding: EdgeInsets.symmetric(horizontal: 16.w),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          widget.showForgotPass
-              ? Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      widget.labelText,
-                      style: Styles.textStyle18.copyWith(
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(right: 8.h),
-                      child: GestureDetector(
-                        onTap: widget.onTap,
-                        child: Text(
-                          'Forgot Password?',
-                          style: Styles.textStyle14
-                              .copyWith(fontWeight: FontWeight.w600),
-                        ),
-                      ),
-                    ),
-                  ],
-                )
-              : Text(
-                  widget.labelText,
-                  style: Styles.textStyle18.copyWith(
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-          Padding(
-            padding: EdgeInsets.symmetric(
-              vertical: 8.h,
-            ),
-            child: Form(
-              key: formKey,
-              child: TextField(
-                textAlignVertical: TextAlignVertical.center,
-                keyboardType: widget.labelText == 'National number' ||
-                        widget.labelText == 'Phone number'
-                    ? TextInputType.number
-                    : TextInputType.text,
-                obscureText: widget.showPassword,
-                controller: controller,
-                onChanged: widget.onChanged,
-                cursorColor: kSecondaryColor.withOpacity(0.8),
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(
-                    horizontal: 8.w,
-                    vertical: 0.h,
-                  ),
-                  suffixIcon: widget.labelText == 'Password' ||
-                          widget.labelText == 'Confirm password'
-                      ? customPasswordIcon()
-                      : null,
-                  hintText: widget.hintText,
-                  hintStyle: Styles.textStyle12.copyWith(
-                    color: kHintColor.withOpacity(0.6),
-                  ),
-                  border: outlineInputBorder(
-                    width: 1.w,
-                    color: kHintColor.withOpacity(0.6),
-                  ),
-                  focusedBorder: outlineInputBorder(
-                    width: 2.w,
-                    color: kSecondaryColor.withOpacity(0.8),
-                  ),
-                  errorBorder: outlineInputBorder(
-                    width: 2.w,
-                    color: kErrorColor.withOpacity(0.8),
-                  ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                widget.labelText,
+                style: Styles.textStyle18.copyWith(
+                  fontWeight: FontWeight.w500,
                 ),
               ),
+              if (widget.showForgotPass)
+                GestureDetector(
+                  onTap: widget.onTap,
+                  child: Text(
+                    "Forgot Password?",
+                    style: Styles.textStyle14.copyWith(
+                      color: kSignIconColor,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+            ],
+          ),
+          SizedBox(height: 8.h),
+          TextFormField(
+            controller: widget.controller,
+            obscureText: showPassword,
+            keyboardType: widget.labelText == 'National number'
+                ? TextInputType.number
+                : TextInputType.text,
+            onChanged: widget.onChanged,
+            validator: widget.validator,
+            decoration: InputDecoration(
+              hintText: widget.hintText,
+              hintStyle: Styles.textStyle12.copyWith(
+                color: Colors.grey[600],
+              ),
+              border: _outlineInputBorder(Colors.grey[600]!),
+              focusedBorder: _outlineInputBorder(kSecondaryColor),
+              errorBorder: _outlineInputBorder(kErrorColor),
+              focusedErrorBorder: _outlineInputBorder(kErrorColor),
+              suffixIcon: widget.obscureText
+                  ? IconButton(
+                      onPressed: () {
+                        setState(() {
+                          showPassword = !showPassword;
+                        });
+                      },
+                      icon: SvgPicture.asset(
+                        showPassword
+                            ? 'assets/images/hide.svg'
+                            : 'assets/images/view.svg',
+                        height: 24.h,
+                        width: 24.w,
+                      ),
+                    )
+                  : null,
             ),
           ),
+          if (widget.errorText != null && widget.errorText!.isNotEmpty) ...[
+            SizedBox(height: 6.h),
+            Row(
+              children: [
+                Icon(
+                  Icons.error_outline,
+                  color: Colors.red,
+                  size: 18.w,
+                ),
+                SizedBox(width: 6.w),
+                Expanded(
+                  child: Text(
+                    widget.errorText!,
+                    style: Styles.textStyle12.copyWith(
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ],
       ),
     );
   }
 
-  IconButton customPasswordIcon() {
-    return IconButton(
-      onPressed: () {
-        setState(() {
-          widget.showPassword = !widget.showPassword;
-        });
-      },
-      icon: SvgPicture.asset(
-        widget.showPassword
-            ? 'assets/images/hide.svg'
-            : 'assets/images/view.svg',
-        height: 24.h,
-        width: 24.w,
-      ),
-    );
-  }
-
-  OutlineInputBorder outlineInputBorder(
-      {required Color color, required double width}) {
+  OutlineInputBorder _outlineInputBorder(Color color) {
     return OutlineInputBorder(
       borderRadius: BorderRadius.circular(10.r),
-      borderSide: BorderSide(
-        color: color,
-        width: width,
-      ),
+      borderSide: BorderSide(color: color, width: 1.5),
     );
   }
 }
+
+
